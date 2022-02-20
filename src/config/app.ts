cca64ed46697189378a7f84045df9dfe3aae4e87
Maikon Parreira <mknparreira@gmail.com>
2022-02-20 22:47:55 +0000

    Enabled express body parser and create middleware function

--- src/config/app.ts
+++ src/config/app.ts
@@ -9,9 +9,7 @@ export default class App {
         routes : Array<express.Router>  
     ) {
         this.app = express();
-        //this.app.use(express.json());
-        //this.app.user(cors());
-        //this.app.use(express.urlencoded({ extended: true }));
+        this.middlewares();
         this.routes(routes);
     }
 
@@ -19,6 +17,15 @@ export default class App {
         routes.forEach((route) => {
             this.app.use(route);
         });
+    }
+
+    private middlewares() {
+        /* I´m using the express.json() and express.urlencoded() middlewares to enable to send a JSON response with res.json() 
+            and parse the URL body and extract data for HTTP METHOD POST data that doesn’t appear in URL in req.body, respectively. 
+        */
+        this.app.use(express.json());
+        this.app.use(express.urlencoded({ extended: true }));
+        //this.app.use(cors());
     }
 
     public listen(port : string, callback : Callback) {
