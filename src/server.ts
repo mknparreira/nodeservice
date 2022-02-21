22fdddafaee32a82f444cdc335e0f651bd1c5627
Maikon Parreira <mknparreira@gmail.com>
2022-02-21 22:56:08 +0000

    Integrated server.ts file with logger

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import App  from './config/app';
 import path from 'path';
 import routes from './config/routes';
 import { ConfigType } from "./interfaces/configType-interface";
+import Logger from './config/logger';
 
 const dirname = path.join(path.resolve(), '/src');
 process.env.NODE_CONFIG_DIR  = dirname + '/config';
@@ -11,5 +12,5 @@ const general: ConfigType = config.get('general');
 const app = new App(routes);
 
 app.listen(<string> general.host.port, () => {
-  return console.log(`Express is listening at http://localhost:${general.host.port}`);
+  Logger.info(`Express is listening at http://localhost:${general.host.port}`);
 });
