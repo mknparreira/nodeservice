921706368fdc7667cfb7de06e5e31a0b844d2170
Maikon Parreira <mknparreira@gmail.com>
2022-03-28 16:28:42 +0100

    Lint issue

--- src/components/user/user-feature.test.ts
+++ src/components/user/user-feature.test.ts
@@ -1,12 +1,10 @@
 import 'jest';
 import * as connection from '../../config/databaseConnection';
 import { UserRepository } from './user-repository';
-
 import { User } from './user-model';
 import { getCustomRepository, getRepository } from 'typeorm';
 
 describe('User Feature', () => {
-
     beforeAll(async () => { 
         await connection.create();
     });
