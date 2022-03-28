de88cec4f9c460bea886ef142d885d6c9d2cd696
Maikon Parreira <mknparreira@gmail.com>
2022-03-28 16:23:53 +0100

    Linter fix and a better log message

--- src/middlewares/errorHandler-middleware.ts
+++ src/middlewares/errorHandler-middleware.ts
@@ -4,14 +4,12 @@ import DefaultException from "../exceptions/default-exception";
 import { ReasonPhrases, StatusCodes } from 'http-status-codes';
 
 function ErrorHandlerMiddleware(error : DefaultException, req: Request, res: Response, next: NextFunction) {
-    if (res.headersSent) {
-        return next(error);
-    }
+    if (res.headersSent) return next(error);
     
     const status  = error.status  || StatusCodes.INTERNAL_SERVER_ERROR;
     const message = error.message || ReasonPhrases.INTERNAL_SERVER_ERROR;
 
-    Logger.error(`StatusCode: ${status} - Message: ${message} - Error: ${JSON.stringify(error.stack, null, 4)}`);
+    Logger.error(`HTTP Status Code: ${status} - Message: ${message} - Error: ${JSON.stringify(error.stack, null, 4)}`);
 
     res.status(status).json({
         message: message,
