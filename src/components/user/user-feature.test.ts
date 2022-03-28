c722fcd7d9fa280a949ce608edc2783981575534
Maikon Parreira <mknparreira@gmail.com>
2022-03-28 16:24:24 +0100

    Added tests for user feature

--- src/components/user/user-feature.test.ts
+++ src/components/user/user-feature.test.ts
@@ -1,23 +1,32 @@
 import 'jest';
+import * as connection from '../../config/databaseConnection';
+import { UserRepository } from './user-repository';
 
-describe('Hello', () => {
-    it('should returns false', async () => {
-        console.log(process.env.NODE_ENV);
-        console.log(process.env.HOST_PORT);
-        const result = false;
-        expect(result).toBe(false);
+import { User } from './user-model';
+import { getCustomRepository, getRepository } from 'typeorm';
+
+describe('User Feature', () => {
+
+    beforeAll(async () => { 
+        await connection.create();
     });
 
-    it('should returns true', async () => {
-        const result = true;
-        expect(result).toBe(true);
+    afterEach(async () => {
+        await connection.clearAll();
     });
 
-    it('display user register', async () => {
-        console.log(process.env.NODE_ENV);
-        console.log(process.env.HOST_PORT);
-        const result = false;
-        expect(result).toBe(false);
+    afterAll(async () => {
+        await connection.close();
     });
+    
+    it('displays user register', async () => {
+        await getRepository(User).insert({
+            firstName: "John",
+            lastName: "Doe",
+            age: 36
+        });
 
+        const users = await getCustomRepository(UserRepository).getUsers();
+        expect(users[0]).toBeInstanceOf(User);
+    });
 });
