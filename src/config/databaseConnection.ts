5d3ae7b60eba1db9040de7f68049ad19d901f7cd
Maikon Parreira <mknparreira@gmail.com>
2022-03-28 16:18:53 +0100

    Added Typeorm database connection

--- src/config/databaseConnection.ts
+++ src/config/databaseConnection.ts
@@ -1,4 +1,4 @@
-import { Connection, createConnection } from 'typeorm';
+import { Connection, createConnection, EntityTarget, getConnection } from 'typeorm';
 import NotFoundException from '../exceptions/notFound-exception';
 import Logger from './logger';
 
@@ -13,8 +13,42 @@ const create = async () : Promise<Connection> => {
     }
 };
 
-const databaseConnection = { 
-    create,
+const getEntities = async () : Promise<{ name: EntityTarget<unknown>; tableName: string; }[]> => {
+    const entities: { name: string; tableName: string; }[] = [];
+    const connection = getConnection();
+    const entityMetadatas = connection.entityMetadatas;
+
+    entityMetadatas.forEach(entity => {
+         entities.push({ name: entity.name, tableName: entity.tableName });
+    });
+    
+    return entities;
 };
-  
-export default databaseConnection;
+
+const clearAll = async () : Promise<void> => {
+    try {
+        const connection = getConnection();
+        const entities = await getEntities();
+       
+        //Note: To read the files in sequence, you cannot use forEach indeed.
+        for (const entity of entities) {
+            const repository = connection.getRepository(entity.name);
+            await repository.clear();
+        }       
+    } catch (error) {
+        Logger.error(error);
+        throw new NotFoundException(`Cleaning test db: ${error}`);
+    }
+};
+
+const close = async () : Promise<void> => {
+    try {
+        const connection = getConnection();
+        if (connection.isConnected) await connection.close();
+    } catch (error) {
+        Logger.error(error);
+        throw new NotFoundException(`Try to close connection: ${error}`);
+    }
+};
+
+export { create, close, clearAll };
