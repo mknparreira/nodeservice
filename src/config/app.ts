1aed3a6d1bb30eef6fd3cc8c50c1f3d9f3cfb7c3
Maikon Parreira <mknparreira@gmail.com>
2022-03-28 16:21:00 +0100

    Removed useless code

--- src/config/app.ts
+++ src/config/app.ts
@@ -10,7 +10,6 @@ export default class App {
         this.app = express();
         this.middlewares();
         this.routes(routes);
-        this.exceptions();
     }
 
     private routes(routes: Array<express.Router>) {
@@ -19,16 +18,13 @@ export default class App {
         });
     }
 
-    private exceptions(): void {
-        this.app.use(ErrorHandlerMiddleware);
-    }
-
     private middlewares(): void {
         /* I´m using the express.json() and express.urlencoded() middlewares to enable to send a JSON response with res.json() 
             and parse the URL body and extract data for HTTP METHOD POST data that doesn’t appear in URL in req.body, respectively. 
         */
         this.app.use(express.json());
         this.app.use(express.urlencoded({ extended: true }));
+        this.app.use(ErrorHandlerMiddleware);
         //this.app.use(cors());
     }
 
