e50f93e2be3ad215b0ae758deabe77362f1559c1
Maikon Parreira <mknparreira@gmail.com>
2022-03-30 10:37:52 +0100

    Fixed a bug when databaseConnection is called

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import path from 'path';
 import routes from './config/routes';
 import { ConfigType } from "./interfaces/configType-interface";
 import Logger from './config/logger';
-import databaseConnection from "./config/databaseConnection";
+import * as connection from "./config/databaseConnection";
 
 const dirname = path.join(path.resolve(), '/src');
 process.env.NODE_CONFIG_DIR  = dirname + '/config';
@@ -13,7 +13,7 @@ import config from 'config';
 const general: ConfigType = config.get('general');
 const app = new App(routes);
 
-databaseConnection.create().then(
+connection.create().then(
   async () => {
     app.listen(<string> general.host.port, () => {
       Logger.info(`Express is listening at http://localhost:${general.host.port}`);
