4c96631de1f8530709f1f65a1949666178bcbf32
Maikon Parreira <mknparreira@gmail.com>
2022-03-31 01:16:44 +0100

    Integrated decorator validation with create method

--- src/components/user/user-controller.ts
+++ src/components/user/user-controller.ts
@@ -1,12 +1,9 @@
-import { plainToClass } from 'class-transformer';
-import { validate } from 'class-validator';
 import { NextFunction, Request, Response } from 'express';
 import { delay, inject, injectable } from 'tsyringe';
-import BadRequestException from '../../exceptions/badRequest-exception';
+import validateRequest from '../../decorators/validateRequest-decorator';
 import NotFoundException from '../../exceptions/notFound-exception';
 import { User } from './user-model';
 import UserService from './user-service';
-
 @injectable()
 export class UserController {
 
@@ -29,19 +26,9 @@ export class UserController {
         } 
     }
 
+    @validateRequest(User)
     async create(req: Request, res: Response, next: NextFunction) : Promise<any> {
         try {
-            const t = plainToClass(User, req.body);
-            const errors = await validate(t);
-
-            if (errors.length > 0) {
-                let errorTexts:any = [];
-                for (const errorItem of errors) {
-                    errorTexts = errorTexts.concat(errorItem.constraints);
-                }
-                throw new BadRequestException(errorTexts);
-            }
-        
             const result = await this.userService.create(req.body);
             return res.json(result);
         } catch (error) {
