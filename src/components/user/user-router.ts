1a9bf0f5c0666048b012c44cf1da6c0b8fa3108e
Maikon Parreira <mknparreira@gmail.com>
2022-04-02 15:35:23 +0100

    Renamed users routes adding user prefix

--- src/components/user/user-router.ts
+++ src/components/user/user-router.ts
@@ -6,6 +6,8 @@ const routes = Router();
 const controller = container.resolve(UserController);
 
 routes.get('/users', controller.getUsers.bind(controller));
-routes.post('/create', controller.create.bind(controller));
+routes.post('/user/create', controller.create.bind(controller));
+routes.put('/user/edit', controller.edit.bind(controller));
+routes.get('/user/show/:vatNumber', controller.list.bind(controller));
 
 export default routes;
