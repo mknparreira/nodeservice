f02a99fffa6d22af58a45dca38ed497b3e73f10e
Maikon Parreira <mknparreira@gmail.com>
2022-04-08 15:19:10 +0100

    Added user-controller.test with supertest

---
+++ src/components/user/user-controlller.test.ts
@@ -0,0 +1,109 @@
+import * as connection from '../../config/databaseConnection';
+import request from 'supertest';
+import App from '../../config/app';
+import routes from '../../components/user/user-router';
+import express from 'express';
+import { StatusCodes } from 'http-status-codes';
+
+describe('User Controller', () => {
+  let app: express.Application;
+
+  beforeAll(async() => {
+    await connection.create();
+
+    app = new App([routes]).app;
+  });
+
+  afterEach(async() => {
+    await connection.clearAll();
+  });
+
+  afterAll(async() => {
+    await connection.close();
+  });
+
+  it('/user/create', async() => {
+    const response = await request(app)
+      .post('/user/create')
+      .set('Accept', 'application/json')
+      .send({
+        'name': 'John Doe',
+        'address': null,
+        'birthDate': '1985-04-01',
+        'email': 'joe.doe@gamil.com',
+        'vat': 30
+      })
+      .expect('Content-Type', 'application/json; charset=utf-8');
+
+    expect(response.body).toBeInstanceOf(Object);
+    expect(response.body.email).toBe('joe.doe@gamil.com');
+    expect(response.body.vat).toBe(30);
+    expect(response.status).toBe(StatusCodes.CREATED);
+  });
+
+  it('/users', async() => {
+    await request(app).post('/user/create').send({
+      'name': 'John Doe',
+      'address': null,
+      'birthDate': '1985-04-01',
+      'email': 'joe.doe@gamil.com',
+      'vat': 30
+    });
+
+    const response = await request(app)
+      .get('/users')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', 'application/json; charset=utf-8');
+
+    expect(response.body).toBeInstanceOf(Array);
+    expect(response.body).toHaveLength(1);
+    expect(response.body[0].email).toBe('joe.doe@gamil.com');
+    expect(response.body[0].vat).toBe('30');
+    expect(response.status).toBe(StatusCodes.OK);
+  });
+
+  it('/user/show/:id', async() => {
+    await request(app).post('/user/create').send({
+      'name': 'John Doe',
+      'address': null,
+      'birthDate': '1985-04-01',
+      'email': 'joe.doe@gamil.com',
+      'vat': 30
+    });
+
+    const response = await request(app)
+      .get('/user/show/30')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', 'application/json; charset=utf-8');
+
+    expect(response.body).toBeInstanceOf(Object);
+    expect(response.body.email).toBe('joe.doe@gamil.com');
+    expect(response.body.vat).toBe('30');
+    expect(response.status).toBe(StatusCodes.OK);
+  });
+
+  it('/user/edit', async() => {
+    await request(app).post('/user/create').send({
+      'name': 'John Doe',
+      'address': null,
+      'birthDate': '1985-04-01',
+      'email': 'joe.doe@gamil.com',
+      'vat': 30
+    });
+
+    const response = await request(app)
+      .put('/user/edit')
+      .set('Accept', 'application/json')
+      .send({
+        'name': 'John Deo Jr',
+        'address': null,
+        'birthDate': '1985-04-01',
+        'email': 'joe.junior@gamil.com',
+        'vat': 30
+      })
+      .expect('Content-Type', 'application/json; charset=utf-8');
+
+    expect(response.body).toBe('The resource successfully updated');
+    expect(response.status).toBe(StatusCodes.OK);
+  });
+});
